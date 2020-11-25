445dd7050d2f79ef132bc740a55f78c8ec631bd5
mal <mal@dev.fios-router.home>
2020-11-24 23:20:10 -0500

    trying to build xapi

---
+++ xapi/dependencies.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e                                                       # exit on any error
+
+set -v                                         # echo all lines before execution
+
+[ `whoami` == 'root' ] && echo 'must NOT be root!' && exit # check for superuser
+
+# -- apk-sourced packages ---------------------------------------------------- #
+
+sudo cp -v ./repositories /etc/apk/repositories       # Add community repository
+
+sudo apk add git opam nano ocaml wget tar make ocaml-compiler-libs        # deps
+
+sudo apk add dune gmp-dev perl pkgconf xen-dev       # needed for making zen-api
+
+sudo apk add libc-dev                               # needed for installing dune
+
+sudo apk add ocaml-ocamldoc                             # needed for zenith, &c.
+
+# stdext xapi-netdev xapi-stdext-monadic
+
+# -- opam-sourced packages --------------------------------------------------- #
+
+opam init                              # Initialize opam. Just keep hitting 'y'.
+
+eval $(opam env)                                               # Initialize opam
+
+opam pin https://github.com/umhau/ppx_custom_printf.git            # fixed a bug
+
+opam install dune base64 ppxlib async js_of_ocaml-ppx lwt       # ocaml-rpc deps
+
+opam install cow cmdliner rresult yojson xmlm              # more ocaml-rpc deps
+
+rm -rf ocaml-rpc                               # in case an old version is there
+
+git clone https://github.com/NathanReb/ocaml-rpc.git   # this dude fixed the bug
+
+cd ocaml-rpc; git checkout d8f0244d5ea3bb13a37a94d5b6749a6fd82ab459; cd .. # fix
+
+opam pin ocaml-rpc#HEAD                              # install the fixed version
+
+opam repo add xs-opam https://github.com/xapi-project/xs-opam.git    # xapi repo
+
+opam install alcotest astring cdrom ctypes ezxenstore gzip http-svr message-switch-unix mtime pciutil ppx_deriving_rpc ppx_sexp_conv rpclib rrdd-plugin sexpr sha stunnel tar tar-unix uuid uuidm x509 xapi-idl xapi-inventory xapi-stdext-date xapi-stdext-encodings cmdliner ocamlfind xapi-stdext-pervasives xapi-stdext-std xapi-stdext-threads xapi-stdext-unix xapi-tapctl xapi-test-utils xapi-xenopsd xenctrl xml-light2 yojson http-svr xapi-idl js_of_ocaml-ppx base64 ppxlib async lwt cow rresult xmlm http-svr ppx_deriving_rpc
+
+# extant failures: systemd, message-switch-core (sometimes...), xapi-inventory
+
+
+# hold then delete
+
+# apk add m4                            # needed for findlib ocaml package manager
+
+# apk add libc-dev ocaml-compiler-libs                   # deps for making findlib
+
+# wget http://download.camlcity.org/download/findlib-1.8.1.tar.gz    # get findlib
+
+# tar xvzf findlib-1.8.1.tar.gz                                          # extract
+
+# cd findlib-1.8.1                                               # enter directory
+
+# ./configure                        # this should figure out a good configuration
+
+# make all               # This creates findlib.cma, findlib_mt.cma, and ocamlfind
+
+# make opt         # This creates findlib.cmxa, findlib_mt.cmxa, and ocamlfind_opt
+
+# make install                              # installs the newly compiled binaries 
+
+# make clean                                       # cleans up the build directory
---
+++ xapi/xapi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e                                                       # exit on any error
+
+set -v                                         # echo all lines before execution
+
+[ `whoami` == 'root' ] && echo 'must NOT be root!' && exit # check for superuser
+
+bash ./dependencies.sh
+
+rm -rf xen-api                                       # remove if already present
+
+git clone https://github.com/xapi-project/xen-api.git # Pull the XAPI repository
+
+cd xen-api
+
+git checkout 5ba6c01e485c61eefb69e520e641a918653ee2d1  # checkout latest release
+
+mv ./configure ./configure.original && cp ../configure ./       # use my version
+
+chmod +x ./configure
+
+./configure                                       # run the configuration script
+
+dune external-lib-deps --missing -j 1 --profile release @install
+
