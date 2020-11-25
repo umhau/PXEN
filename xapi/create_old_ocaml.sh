2a7d04026c7bd993847dde1ca08c76c89b68fe57
mal <mal@dev.fios-router.home>
2020-11-25 17:03:52 -0500

    edit

--- xapi/create_old_ocaml.sh
+++ xapi/create_old_ocaml.sh
@@ -1,7 +1,22 @@
 
 sudo apk add alpine-sdk   # metapackage pulls packages for building new packages
 
-[ -d aports ] || git clone git://git.alpinelinux.org/aports  # clone aports tree
+# [ -d aports ] || git clone git://git.alpinelinux.org/aports  # clone aports tree
 
-cd aports/community/ocaml
+# cd aports/community/ocaml
 
+# that's how I got the ocaml folder, but it's a one-time deal
+
+cd ocaml
+
+sudo mkdir -p /var/cache/distfiles 
+
+sudo chmod a+w /var/cache/distfiles
+
+abuild-keygen -a -i
+
+sudo addgroup `whoami` abuild
+
+abuild checksum
+
+abuild -r
