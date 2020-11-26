2930f87778d2682f66d94286da59304f8e05f83c
mal <mal@dev.fios-router.home>
2020-11-25 19:19:12 -0500

    alteration

--- xapi/dependencies.sh
+++ xapi/dependencies.sh
@@ -4,7 +4,9 @@ set -e                                                       # exit on any error
 
 set -v                                         # echo all lines before execution
 
-ocaml_version='4.04.1'
+# ocaml_version='4.04.1'
+
+ocaml_version='4.02.3'
 
 [ `whoami` == 'root' ] && echo 'must NOT be root!' && exit # check for superuser
 
